0afb700f05c17d5ca098a98c31610d0529c8f310
MaazMohammedM <85998398+MaazMohammedM@users.noreply.github.com>
2026-02-04 22:48:46 +0530

    add dynamic search functionality for weather search

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 let key = 'ee5540aedbbf40ca93d170220260202';
+let searchBtn = document.getElementById('searchBtn');
+let searchContainer = document.querySelector('.searchContainer');
 
-const getWeather = async () => {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?q=ambur&key=${key}`, {
+
+const getWeather = async (query) => {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?q=${query}&key=${key}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         }
     });
     const data = await response.json();
-    renderSearchUI(data.location.name, data.location.region, data.location.country,data.current.condition.text,data.current.condition.icon,data.current.temp_c,data.current.feelslike_c,data.current.humidity,data.current.wind_kph);
-    console.log(data)
+    return data;
 }
 
-const renderSearchUI =(placeName,placeRegion,placeCountry,placeCondition,imageLink,placeCurrentTemp,placeFeelsLikeTemp, placeHumidity,placeWind)=>{
+const getSearchedWeather = async () => {
+    const initalContainer = document.querySelector('.initalContainer');
+    const searchContainer = document.querySelector('.searchContainer');
+    const loading = document.querySelector('.loading');
+    try {
+        const query = document.getElementById('searchWeather').value;
+        if (query.length < 2) {
+            alert("City name less then 2 characters");
+            return
+        } else {
+            initalContainer.style.display = 'none';
+            searchContainer.style.display = 'none';
+            loading.style.display = "block";
+            const data = await getWeather(query);
+            loading.style.display = 'none';
+            searchContainer.style.display = 'block';
+            renderSearchUI(data.location.name, data.location.region, data.location.country, data.current.condition.text, data.current.condition.icon, data.current.temp_c, data.current.feelslike_c, data.current.humidity, data.current.wind_kph);
+            console.log(data)
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const renderSearchUI = (placeName, placeRegion, placeCountry, placeCondition, imageLink, placeCurrentTemp, placeFeelsLikeTemp, placeHumidity, placeWind) => {
     const name = document.querySelector('.placeName');
     const region = document.querySelector('.placeRegion');
     const country = document.querySelector('.placeCountry');
@@ -35,4 +62,74 @@ const renderSearchUI =(placeName,placeRegion,placeCountry,placeCondition,imageLi
 
 }
 
-getWeather();
+const getLocation = () => {
+    return new Promise((resolve, reject) => {
+        if (!navigator.geolocation) {
+            reject(new Error("Location is not supported in your browser!"));
+        }
+        const retrivingLocation = (position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+
+            resolve({
+                latitude: latitude,
+                longitude: longitude
+            })
+        }
+        const errorCallback = (error) => {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    reject(new Error("User denied the request for geolocation."));
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    reject(new Error("Location information is unavailable."));
+                    break;
+                case error.TIMEOUT:
+                    reject(new Error("The request to get user location timed out."));
+                    break;
+                case error.UNKNOWN_ERROR:
+                    reject(new Error("An unknown error occurred."));
+                    break;
+            }
+        }
+        navigator.geolocation.getCurrentPosition(retrivingLocation, errorCallback);
+    })
+
+}
+
+
+const renderCurrentUI =(name,region,country,link,message)=>{
+    const cName = document.querySelector('.cName');
+    const currentRegion = document.querySelector('.currentRegion');
+    const currentCountry = document.querySelector('.currentCountry');
+    const currentConditionImg = document.querySelector('.currentConditionImg');
+    const currentConditionMsg = document.querySelector('.currentConditionMsg');
+
+    cName.textContent = name;
+    currentRegion.textContent = region;
+    currentCountry.textContent = country;
+    currentConditionImg.src = link;
+    currentConditionMsg.textContent = message;
+}
+
+const getCurrentWeather = async () => {
+    try {
+        const cordinates = await getLocation();
+        let latLong = `${cordinates.latitude},${cordinates.longitude}`;
+        const currentWeather = await getWeather(latLong);
+        console.log(currentWeather);
+        renderCurrentUI(currentWeather.location.name,currentWeather.location.region,
+        currentWeather.location.country,currentWeather.current.condition.icon,currentWeather.current.condition.text)
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+searchBtn.addEventListener('click', () => {
+    getSearchedWeather();
+})
+
+
+getCurrentWeather();
